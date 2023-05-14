faa7ac32a386d21e27d3c9f7e8e7b4c8b3f4654e
Timur <timurtokaev.toombez@gmail.com>
2023-05-15 00:52:21 +0300

    Add tailwind css

---
+++ web-development/web/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    mode: 'jit',
+    content: ["./templates/**/*.{html,htm,j2}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
