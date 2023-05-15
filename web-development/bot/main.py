6eb4d18f3a1a9602d29b7e541bc5e7c359aa118c
Timur <timurtokaev.toombez@gmail.com>
2023-05-15 18:51:16 +0300

    Start bot

--- web-development/bot/main.py
+++ web-development/bot/main.py
@@ -1,5 +1,12 @@
 from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, executor, types
 import os
 
 load_dotenv()
 TOKEN = os.environ['TOKEN']
+
+bot = Bot(TOKEN)
+
+dp = Dispatcher(bot)
+
+executor.start_polling(dp)
