6c5461ddf402b682b1a10be22d35cb265e79426e
Timur <timurtokaev.toombez@gmail.com>
2023-05-15 16:55:10 +0300

    Add news page

--- web-development/web/app.py
+++ web-development/web/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.j2')
 
+@app.route('/news')
+def news():
+    return render_template('news.j2')
+
 if __name__ == '__main__':
     app.run(debug=True)
