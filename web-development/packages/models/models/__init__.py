5a32e86abfc84a3603a268d656d22ca87a5f09be
Timur <timurtokaev.toombez@gmail.com>
2023-05-22 21:52:44 +0300

    Export models

---
+++ web-development/packages/models/models/__init__.py
@@ -0,0 +1,5 @@
+from models.News import News
+from models.Theme import Theme
+from models.Module import Module
+from models.ThemeMaterial import ThemeMaterial
+from models.ThemeSkill import ThemeSkill
