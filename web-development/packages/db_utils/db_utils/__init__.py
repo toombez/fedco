0e93ccf35c672c61f580633a06d6eefcf131087a
Timur <timurtokaev.toombez@gmail.com>
2023-05-23 00:54:08 +0300

    Add db_utils package

---
+++ web-development/packages/db_utils/db_utils/__init__.py
@@ -0,0 +1,30 @@
+from models import News, Module, Theme, ThemeMaterial, ThemeSkill
+
+def fetch_news():
+    news = News.select()
+
+    return tuple(map(lambda n: (n.heading, n.content, n.image_url), news))
+
+def fetch_modules():
+    modules = Module.select()
+
+    return tuple(map(
+        lambda m: (
+            m.name,
+            tuple(map(
+                lambda t: (
+                    t.name,
+                    tuple(map(
+                        lambda s: s.label,
+                        ThemeSkill.select().where(ThemeSkill.theme_id == t.id)
+                    )),
+                    tuple(map(
+                        lambda material: (material.label, material.href),
+                        ThemeMaterial.select().where(ThemeMaterial.theme_id == t.id)
+                    ))
+                ),
+                Theme.select().where(Theme.module_id == m.id)
+            ))
+        ),
+        modules
+    ))
