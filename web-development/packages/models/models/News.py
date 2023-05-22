a2241dd19e10df6aa247d5252707e0f0479f107d
Timur <timurtokaev.toombez@gmail.com>
2023-05-22 21:40:30 +0300

    Add News model

--- web-development/packages/models/models/News.py
+++ web-development/packages/models/models/News.py
@@ -1,36 +1,7 @@
-from typing import Union, Tuple
-from dataclasses import dataclass
-from models.Model import Model
+from models.BaseModel import BaseModel
+from peewee import CharField
 
-@dataclass
-class News(Model):
-    heading: str
-    content: str
-    image_url: Union[str, None] = None
-
-    @staticmethod
-    def from_sql(sql: Tuple[str, str, Union[str, None]]):
-        """
-        Create `News` model from sql result
-
-        Parameters
-        ----------
-            sql: Tuple[str, str, Union[str, None]]
-                (heading, content, image_url)
-
-        Returns
-        -------
-            `News` model
-        """
-        heading, content, image_url = sql
-        return News(heading, content, image_url)
-
-    def to_sql(self):
-        """
-        Pass `News` to sql
-
-        Returns
-        -------
-            Tuple (heading, content, image_url)
-        """
-        return (self.heading, self.content, self.image_url)
+class News(BaseModel):
+    heading = CharField()
+    content = CharField()
+    image_url = CharField(null=True)
