427137a57e4f024859b6e918a5c91c5608f25a1f
Timur <timurtokaev.toombez@gmail.com>
2023-05-22 03:59:23 +0300

    Add scripts to create tables

---
+++ web-development/packages/db/db/sql/create_modules_materials_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS modules_materials (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    label TEXT,
+    href TEXT,
+    module_id INTEGER
+)
---
+++ web-development/packages/db/db/sql/create_modules_skills_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS modules_skills (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    label TEXT,
+    module_id INTEGER
+)
